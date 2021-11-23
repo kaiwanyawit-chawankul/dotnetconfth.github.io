c8d3f933233fbc515aef8f191ef68fff59d1a37a
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-11-23 17:15:05 +0700

    send to lhci server

--- local.js
+++ local.js
@@ -13,9 +13,12 @@ module.exports = {
       },
       upload: {
         /* Add configuration here */
-        target: 'lhci',
-        serverBaseUrl: 'https://peaceful-earth-32428.herokuapp.com/',
-        token: 'LHCI_TOKEN', // could also use LHCI_TOKEN variable instead
+        //target: 'lhci',
+        //serverBaseUrl: 'https://peaceful-earth-32428.herokuapp.com/',
+        //token: 'LHCI_TOKEN', // could also use LHCI_TOKEN variable instead
+        //target: 'filesystem',
+        //outputDir:'./lhci'
+        target: 'temporary-public-storage',
       },
     },
   };
